54f182a3fafd43ff5126724d3b8ca0aa52ccdd28
Tony Sulfaro <tony116523@gmail.com>
2020-03-21 14:47:33 -0400

    linting errors

--- server.js
+++ server.js
@@ -51,9 +51,9 @@ app.post('/todos', async function (request, response) {
   response.redirect('/todos/' + id)
   client.release()
 })
-app.put("/todos/:id", async function (request, response) {
+app.put('/todos/:id', async function (request, response) {
   if (request.body.title === undefined || request.body.completed === undefined) {
-    response.status(400).end("title and completed are required")
+    response.status(400).end('title and completed are required')
     return
   }
 
@@ -63,13 +63,13 @@ app.put("/todos/:id", async function (request, response) {
 
   var client = await pool.connect()
   var result = await client.query(
-    "update todos set title = $2, completed = $3 where id = $1 returning *",
+    'update todos set title = $2, completed = $3 where id = $1 returning *',
     [id, title, completed]
   )
   if (result.rows.length === 1) {
     response.json(result.rows[0])
   } else {
-    response.status(404).end("todo " + request.params.id + " not found")
+    response.status(404).end('todo ' + request.params.id + ' not found')
   }
   client.release()
 })
