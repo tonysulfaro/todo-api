bb87dd125c689b4f89208779b8672312f05b016d
Tony Sulfaro <tony116523@gmail.com>
2020-03-21 14:36:42 -0400

    can update todo completion status

--- server.js
+++ server.js
@@ -51,32 +51,28 @@ app.post('/todos', async function (request, response) {
   response.redirect('/todos/' + id)
   client.release()
 })
-// app.put("/products/:id", async function(request, response) {
-//   if (request.body.name === undefined || request.body.price === undefined) {
-//     response.status(400).end("name and price are required");
-//     return;
-//   }
+app.put("/todos/:id", async function (request, response) {
+  if (request.body.title === undefined || request.body.completed === undefined) {
+    response.status(400).end("title and completed are required")
+    return
+  }
 
-//   var id = request.params.id;
-//   var name = request.body.name.trim();
-//   var slug = name
-//     .toLowerCase()
-//     .split(" ")
-//     .join("-");
-//   var price = parseFloat(request.body.price).toFixed(2);
+  var id = request.params.id
+  var title = request.body.title.trim()
+  var completed = request.body.completed
 
-//   var client = await pool.connect();
-//   var result = await client.query(
-//     "update product set slug = $2, name = $3, price = $4 where id = $1 returning *",
-//     [id, slug, name, price]
-//   );
-//   if (result.rows.length === 1) {
-//     response.json(result.rows[0]);
-//   } else {
-//     response.status(404).end("product " + request.params.id + " not found");
-//   }
-//   client.release();
-// });
+  var client = await pool.connect()
+  var result = await client.query(
+    "update todos set title = $2, completed = $3 where id = $1 returning *",
+    [id, title, completed]
+  )
+  if (result.rows.length === 1) {
+    response.json(result.rows[0])
+  } else {
+    response.status(404).end("todo " + request.params.id + " not found")
+  }
+  client.release()
+})
 app.delete('/todos/:id', async function (request, response) {
   var client = await pool.connect()
   var result = await client.query('select * from todos where id = $1', [
